53abb550f479cefd7b2628a9737639a0fbf57edb
Gilles <gilles.vanhaecke-freelancer@digitalcareerinstitute.org>
2022-09-15 13:16:05 +0200

    comments

--- routes/exampleRoutes.js
+++ routes/exampleRoutes.js
@@ -9,6 +9,14 @@ router.get('/',(req, res) => {
     return res.status(200).send("welcome to the example routes");
 });
 
+//http://localhost:3000/examples/validateuser 
+//BODY example
+/**
+ * {
+	"email":"MaRktEst@googlemail.com",
+	"password":"testing"
+    }
+ */
 router.post('/validateuser', body('email').isEmail(),body('password').isLength({min:5}),(req, res) => {
 
     const errors = validationResult(req);
@@ -29,6 +37,15 @@ router.post('/validateuser', body('email').isEmail(),body('password').isLength({
     return res.status(200).json({message:'User was added', userObject:user});
 
 });
+
+//http://localhost:3000/examples/sanitizeuser 
+//BODY example
+/**
+ * {
+	"email":"MaRktEst@googlemail.com",
+	"password":"testing"
+    }
+ */
 
 router.post('/sanitizeuser', body('email').isEmail().normalizeEmail(),(req, res) => {
 
@@ -53,6 +70,13 @@ router.post('/sanitizeuser', body('email').isEmail().normalizeEmail(),(req, res)
 
 //trim removes whitespaces around the value
 //try with your phone
+//http://localhost:3000/examples/checkmobilephone 
+//BODY example
+/**
+ * {
+	"phonenumber":"+491711234567"
+   }
+ */
 router.post('/checkmobilephone',body('phonenumber').trim().isMobilePhone(['de-DE']),(req, res) => {
     const errors = validationResult(req);
     //if there are errors (errors is not empty)
@@ -66,6 +90,13 @@ router.post('/checkmobilephone',body('phonenumber').trim().isMobilePhone(['de-DE
     return res.status(200).json(phonenumber)
 })
 
+//http://localhost:3000/examples/statement 
+//BODY example
+/**
+ *{
+	"statement":"SELECT * FROM dbo.users t WHERE t.name = 'test' AND t.name < 4"
+}
+ */
 router.post('/statement',body('statement').escape(),(req, res) => {
     const errors = validationResult(req);
     //if there are errors (errors is not empty)
